559e6b29e2057bb2a4f34aace79e7daf4b6ce3dd
yangyang <yangyang@crowddigital.cn>
2026-06-01 15:10:08 +0800

    fix: use PrismaPg adapter with generated client

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../generated/prisma'
 import { PrismaPg } from '@prisma/adapter-pg'
 import dayjs from 'dayjs'
 import bcrypt from 'bcryptjs'
